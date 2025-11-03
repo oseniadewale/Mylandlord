190e6fcaed8dda03ca6eef2137ad5695c2f5c0ba
osenibabanla <oseni246femi@gmail.com>
2025-11-03 18:06:43 +0100

    Integrated Composer and Dotenv for secure configuration

--- classes/config.php
+++ classes/config.php
@@ -1,15 +1,15 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-if (file_exists(__DIR__ . '/../.env')) {
-    $env = parse_ini_file(__DIR__ . '/../.env');
-    foreach ($env as $key => $value) {
-        putenv("$key=$value");
-    }
-}
+use Dotenv\Dotenv;
 
-// 2️⃣ Define constants (works locally or in Railway)
-define('DBHOST', getenv('DBHOST') ?: 'localhost');
-define('DBNAME', getenv('DBNAME') ?: 'mylandlord');
-define('DBUSER', getenv('DBUSER') ?: 'root');
-define('DBPASS', getenv('DBPASS') ?: '');
+// Load .env file
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Define constants from .env variables
+define('DBHOST', $_ENV['DBHOST']);
+define('DBNAME', $_ENV['DBNAME']);
+define('DBUSER', $_ENV['DBUSER']);
+define('DBPASS', $_ENV['DBPASS']);
 ?>
---
+++ test_env.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo "DB Name: " . $_ENV['DBNAME'];
