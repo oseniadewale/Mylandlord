24c64c4c65801d7f5711a0eac7ac68b043d2b3be
oseniadewale <161083294+oseniadewale@users.noreply.github.com>
2025-11-08 13:23:23 +0100

    Clean up config.php by removing unused code
    Removed commented-out code for dotenv and database configuration.

--- classes/config.php
+++ classes/config.php
@@ -1,27 +1,4 @@
 
-// include_once __DIR__ . '/../vendor/autoload.php';
-
-// use Dotenv\Dotenv;
-
-// // Load .env if it exists
-// $envPath = __DIR__ . '/../';
-// if (file_exists($envPath . '.env')) {
-//     $dotenv = Dotenv::createImmutable($envPath);
-//     $dotenv->load();
-// }
-
-// // If app is local (not on Railway), use the public proxy host
-// $defaultHost = (getenv('RAILWAY_ENVIRONMENT') || getenv('RAILWAY_PROJECT_ID'))
-//     ? 'mysql.railway.internal'
-//     : 'ballast.proxy.rlwy.net';
-
-// define('DBHOST', getenv('DBHOST') ?: $defaultHost);
-// define('DBPORT', getenv('DBPORT') ?: '35446');
-// define('DBNAME', getenv('DBNAME') ?: 'railway');
-// define('DBUSER', getenv('DBUSER') ?: 'root');
-// define('DBPASS', getenv('DBPASS') ?: 'GcYDdKWnzRwiZLLRoMWqlhNaEIqRBqUn');
-
-
 <?php
 define('DBHOST', 'ballast.proxy.rlwy.net');
 define('DBPORT', '35446');
@@ -29,4 +6,6 @@ define('DBNAME', 'railway');
 define('DBUSER', 'root');
 define('DBPASS', 'GcYDdKWnzRwiZLLRoMWqlhNaEIqRBqUn');
 ?>
+
+
 
