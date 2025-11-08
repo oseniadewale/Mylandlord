02b9cdafc504f5ef84ffdf96ef7af31143f1f119
oseniadewale <161083294+oseniadewale@users.noreply.github.com>
2025-11-08 14:34:39 +0100

    Update config.php

--- classes/config.php
+++ classes/config.php
@@ -4,7 +4,7 @@ define('DBHOST', 'mysql.railway.internal');
 define('DBPORT', '3306');
 define('DBNAME', 'railway');
 define('DBUSER', 'root');
-define('DBPASS', 'GcYDdKWnzRwiZLLRoMWqlhNaEIqRBqUn');
+define('DBPASS', 'bbNTSXefERHPQNbEOZHoiYcbfJtCPjTw');
 ?>
 
 
