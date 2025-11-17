615b7df9df8eeae8529b8412be1ba85d45d04cd4
oseniadewale <161083294+oseniadewale@users.noreply.github.com>
2025-11-17 15:46:14 +0100

    Add default DB configuration values
    Updated database configuration to provide default values if environment variables are not set.

--- classes/config.php
+++ classes/config.php
@@ -1,7 +1,5 @@
-<?php
-define('DBHOST', getenv('DBHOST'));
-define('DBPORT', getenv('DBPORT'));
-define('DBNAME', getenv('DBNAME'));
-define('DBUSER', getenv('DBUSER'));
-define('DBPASS', getenv('DBPASS'));
-?>
+define('DBHOST', getenv('DBHOST') ?: 'bw5txopdnwwvozya7rkm-mysql.services.clever-cloud.com');
+define('DBPORT', getenv('DBPORT') ?: '3306');
+define('DBNAME', getenv('DBNAME') ?: 'bw5txopdnwwvozya7rkm'); // your actual DB name
+define('DBUSER', getenv('DBUSER') ?: 'uj0yknnecdzpsedb');
+define('DBPASS', getenv('DBPASS') ?: 'your_password_here');
