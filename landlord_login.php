d42f3a9385c2eaa0356eda7b5c57bb22b043945e
oseniadewale <161083294+oseniadewale@users.noreply.github.com>
2025-11-17 17:17:19 +0100

    Update landlord_login.php

--- landlord_login.php
+++ landlord_login.php
@@ -2,7 +2,7 @@
 session_start();
 $base_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 
-include_once __DIR__ . "/header.php";
+include_once __DIR__ ."/header.php";
 
 
 
