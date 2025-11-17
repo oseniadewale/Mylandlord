4c4af500fd29958ddae1a4d2f5ab4fec543442a6
oseniadewale <161083294+oseniadewale@users.noreply.github.com>
2025-11-17 15:49:47 +0100

    Update config.php

--- classes/config.php
+++ classes/config.php
@@ -1,5 +1,5 @@
 define('DBHOST', getenv('DBHOST') ?: 'bw5txopdnwwvozya7rkm-mysql.services.clever-cloud.com');
 define('DBPORT', getenv('DBPORT') ?: '3306');
-define('DBNAME', getenv('DBNAME') ?: 'bw5txopdnwwvozya7rkm'); // your actual DB name
+define('DBNAME', getenv('DBNAME') ?: 'bw5txopdnwwvozya7rkm'); 
 define('DBUSER', getenv('DBUSER') ?: 'uj0yknnecdzpsedb');
-define('DBPASS', getenv('DBPASS') ?: 'your_password_here');
+define('DBPASS', getenv('DBPASS') ?: 'RmX9RJvH2rmKJFstv7sc');
